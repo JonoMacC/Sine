e3bf7366834cd3b76506552e03a3f738d5830474
JonoMacC <39803935+JonoMacC@users.noreply.github.com>
2020-02-14 23:57:58 -0500

    Add content

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header />
+    <p>
+      Sine is about discovering new music and the people who love it the way you
+      do. We believe that people and music are better together. We are building
+      Sine to make that happen in a way that is fluid, friendly, and delightful.{" "}
+    </p>
+    <p>Sine is being built for iOS and Android.</p>
+    <p>Want to be part of the wave?</p>
+  </div>
+)
